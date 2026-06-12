6ae930ddd7cca82bfe2caf12d7c55370eb77aab7
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-06-12 14:41:51 -0400

    .

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,3 +3,19 @@ mod terminal_browser;
 
 #[cfg(feature = "browser")]
 pub use terminal_browser::{run_browser, run_browser_from_env};
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn package_name_matches_crate() {
+        assert_eq!(env!("CARGO_PKG_NAME"), "fsspec_browser");
+    }
+
+    #[test]
+    fn package_description_matches_scope() {
+        let description = env!("CARGO_PKG_DESCRIPTION");
+
+        assert!(description.contains("fsspec"));
+        assert!(description.contains("browser"));
+    }
+}
